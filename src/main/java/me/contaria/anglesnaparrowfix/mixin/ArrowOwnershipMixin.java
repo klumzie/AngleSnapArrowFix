dacd32a3d0c284018fa84021509eeba023930794
klumzie <36917461+klumzie@users.noreply.github.com>
2025-07-24 13:47:49 +0100

    Update ArrowOwnershipMixin.java

--- src/main/java/me/contaria/anglesnaparrowfix/mixin/ArrowOwnershipMixin.java
+++ src/main/java/me/contaria/anglesnaparrowfix/mixin/ArrowOwnershipMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.nbt.NbtHelper;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
@@ -24,33 +23,27 @@ public abstract class ArrowOwnershipMixin extends Entity {
         super(type, world);
     }
 
-    @Inject(
-        method = "writeCustomDataToNbt(Lnet/minecraft/nbt/NbtCompound;)V",
-        at = @At("TAIL")
-    )
-    private void writeOwnerToNbt(NbtCompound nbt, CallbackInfo ci) {
+    @Inject(method = "writeNbt", at = @At("TAIL"))
+    private void onWriteNbt(NbtCompound nbt, CallbackInfo ci) {
         if (ownerUUID != null) {
-            nbt.put("OwnerUUID", NbtHelper.fromUuid(ownerUUID));
+            nbt.putUuid("OwnerUUID", ownerUUID);
         }
     }
 
-    @Inject(
-        method = "readCustomDataFromNbt(Lnet/minecraft/nbt/NbtCompound;)V",
-        at = @At("TAIL")
-    )
-    private void readOwnerFromNbt(NbtCompound nbt, CallbackInfo ci) {
-        if (nbt.contains("OwnerUUID")) {
-            this.ownerUUID = NbtHelper.toUuid(nbt.get("OwnerUUID"));
+    @Inject(method = "readNbt", at = @At("TAIL"))
+    private void onReadNbt(NbtCompound nbt, CallbackInfo ci) {
+        if (nbt.containsUuid("OwnerUUID")) {
+            this.ownerUUID = nbt.getUuid("OwnerUUID");
         }
-    }
-
-    @Unique
-    public void setOwnerUUID(UUID uuid) {
-        this.ownerUUID = uuid;
     }
 
     @Unique
     public UUID getOwnerUUID() {
-        return this.ownerUUID;
+        return ownerUUID;
+    }
+
+    @Unique
+    public void setOwnerUUID(UUID ownerUUID) {
+        this.ownerUUID = ownerUUID;
     }
 }
